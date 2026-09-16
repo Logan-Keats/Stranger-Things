81d80b511e85ca81d1ae9295315cf753ee012f1a
Yunchho <yunchhohang.nembang@connect.qut.edu.au>
2026-09-16 21:32:48 +1000

    Add resource detail UI and update resource screens

---
+++ src/main/java/com/strangerthings/ResourceDetailController.java
@@ -0,0 +1,16 @@
+package com.strangerthings;
+
+import javafx.fxml.FXML;
+
+public class ResourceDetailController {
+
+    @FXML
+    private void onEditResource() {
+        System.out.println("Edit Resource clicked");
+    }
+
+    @FXML
+    private void onRemoveResource() {
+        System.out.println("Remove Resource clicked");
+    }
+}
