c6aba18dfcaa126edd26451d80f6015221b57b09
Yunchho <yunchhohang.nembang@connect.qut.edu.au>
2026-09-17 22:30:07 +1000

    Expand resource validation tests

--- src/test/java/com/strangerthings/ResourceServiceTest.java
+++ src/test/java/com/strangerthings/ResourceServiceTest.java
@@ -35,4 +35,45 @@ public class ResourceServiceTest {
 
         assertFalse(result);
     }
+
+    @Test
+    void missingDescriptionShouldBeRejected() {
+        ResourceService service = new ResourceService();
+
+        boolean result = service.isValidResource(
+                "Drill",
+                "",
+                "Tools",
+                "Pick Up"
+        );
+
+        assertFalse(result);
+    }
+    @Test
+    void missingCategoryShouldBeRejected() {
+        ResourceService service = new ResourceService();
+
+        boolean result = service.isValidResource(
+                "Drill",
+                "Cordless power drill",
+                null,
+                "Pick Up"
+        );
+
+        assertFalse(result);
+    }
+
+    @Test
+    void missingCollectionMethodShouldBeRejected() {
+        ResourceService service = new ResourceService();
+
+        boolean result = service.isValidResource(
+                "Drill",
+                "Cordless power drill",
+                "Tools",
+                null
+        );
+
+        assertFalse(result);
+    }
 }
